d59a5c2a7b6d7bac3285a1a3b95ef9e376dd67a6
zetsumeishi <olivier.loustaunau@gmail.com>
2021-05-11 20:02:44 +0200

    First commit

---
+++ README.md
@@ -0,0 +1,52 @@
+# Geoguessr Hack
+
+## Disclaimer
+
+This project is for educational purposes only. I'm not responsible for your actions.
+
+## Introduction
+
+POC of a Geoguessr hack using a MITM proxy. It was made using Python 3.8 and [mitmproxy](https://mitmproxy.org/).
+
+## Game Modes
+
+### Standard
+
+In **Standard** games, the full address will be printed and it opens in a new browser tab Google Maps at this address.
+
+### Streak
+
+In **US State Streak**, the state will be printed with the abbreviation (`Texas, TX`)
+
+In **Country Streak**, the country name will be printed.
+
+### Battle Royale
+
+In **Battle Royale**, you get the same feedback as in **Standard** mode.
+
+The difference with the other modes, is that you need to refresh the page when a new round starts.
+The reason for that is that in unranked modes, the position of the current round is sent back by the server automatically when a new round starts.
+When you play in **Battle Royale**, you do not get the location, probably by design. But when you refresh the page, it reconnects you to the game, and sends you the current round. Maybe assuming you got disconnected.
+
+Samples of the different responses sent back by the servers can be found in the folder `response-samples`.
+
+## How to
+
+```shell
+# Installation of the project
+git clone git@github.com:zetsumeishi/geoguessr-hack.git
+cd geoguessr/
+virtualenv -p python3.8 .venv
+source .venv/bin/active
+pip install -r requirements.txt
+```
+
+To use `mitmproxy`, you need to add to your browser the proxy server (`127.0.0.1:8080`). `mitmproxy` is used to intercept every request, include XHR.
+
+Once it's done, the only thing left is to launch `mitmproxy` using the following command.
+
+```shell
+mitmdump --quiet -s geohack.py "~u geoguessr\.com" 2>error
+```
+
+To avoid receiving too many errors in the terminal, we redirect stderr output to a file. Goes for Python and `mitmproxy` errors. A better use of `mitmproxy` CLI options would remove unnecessary requests from being evaluated.
---
+++ geohack.py
@@ -0,0 +1,63 @@
+#! /usr/bin/env python3
+import json
+import re
+import webbrowser
+
+import pycountry
+import us
+from geopy.geocoders import Nominatim
+
+LANGUAGE = 'en'
+
+
+def response(flow):
+    """Called when a server response has been received."""
+
+    unranked_url = r'https://www.geoguessr.com/api/v3/games/\w{16}'
+    ranked_url_pattern = r'^https://game-server.geoguessr.com/api/battle-royale/[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12}/reconnect$'  # NOQA
+
+    is_unranked = re.match(unranked_url, flow.request.url) is not None
+    is_ranked = re.match(ranked_url_pattern, flow.request.url) is not None
+    if flow.request.method != 'GET' or not is_ranked and not is_unranked:
+        return
+
+    # We retrieve the response content in a dict
+    response_content = json.loads(flow.response.content.decode())
+    geolocator = Nominatim(user_agent='Geoguessr')
+    if response_content:
+        if is_unranked:
+            game_mode = response_content['mode']
+            streak_type = response_content['streakType']
+            round_index = response_content['round'] - 1
+            current_round = response_content['rounds'][round_index]
+            latitude = current_round['lat']
+            longitude = current_round['lng']
+            coordinates = (latitude, longitude)
+            webbrowser.open_new_tab(
+                f'https://www.google.com/maps/place/{latitude},{longitude}',
+            )
+            if game_mode == 'streak':
+                if streak_type == 'usstatestreak':
+                    state = us.states.lookup(current_round['streakLocationCode'])
+                    print(f'{state.name}, {state.abbr}')
+                elif streak_type == 'countrystreak':
+                    country_code = current_round['streakLocationCode']
+                    country = pycountry.countries.get(alpha_2=country_code)
+                    print(country.name)
+            elif game_mode == 'standard':
+                location = geolocator.reverse(coordinates, language=LANGUAGE)
+                print(location.address)
+        elif is_ranked:
+            current_round = response_content['rounds'][-1]
+            latitude = current_round['lat']
+            longitude = current_round['lng']
+            coordinates = (latitude, longitude)
+            webbrowser.get(using='google-chrome').open_new_tab(
+                f'https://www.google.com/maps/place/{latitude},{longitude}',
+            )
+            if response_content['isDistanceGame']:
+                location = geolocator.reverse(coordinates, language=LANGUAGE)
+                print(location.address)
+            else:
+                location = geolocator.reverse(coordinates, language=LANGUAGE)
+                print(location.address)
