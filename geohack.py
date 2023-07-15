4499ce0646771c58cdd24586a746afc4a0a8ca59
zetsumeishi <zetsumeishi@protonmail.com>
2023-07-16 00:39:51 +0200

    feat: Update and fix code for current Geoguessr version

--- geohack.py
+++ geohack.py
@@ -1,82 +1,61 @@
 #! /usr/bin/env python3
 import json
 import re
-import webbrowser
 
-import pycountry
-import us
 from geopy.geocoders import Nominatim
 
 LANGUAGE = 'en'
 
 
+def display_location(location):
+    address = location.raw['address']
+    for key, value in sorted(address.items()):
+        # Only display useful information, ignore the rest
+        if 'ISO3166' not in key and key not in ['postcode', 'country_code']:
+            key = ' '.join(key.split('_'))
+            print(f'\033[1m{key.title()}\033[0m: {value.title()}')
+    print('\n')
+
+
 def response(flow):
-    """Called when a server response has been received."""
+    url = flow.request.pretty_url
+    adventure_pattern = r'^https:\/\/www\.geoguessr\.com\/api\/v4\/adventures\/[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12}\/move\/(\d+)'  # NOQA
+    std_pattern = (
+        r'^https:\/\/www\.geoguessr\.com\/api\/v3\/(games|challenges)\/[A-Za-z0-9]{16}'  # NOQA
+    )
 
-    unranked_url = r'https://www.geoguessr.com/api/v3/games/\w{16}$'
-    ranked_url_pattern = r'^https://game-server.geoguessr.com/api/battle-royale/[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12}/reconnect$'  # NOQA
-    challenges_url_pattern = r'https://www.geoguessr.com/api/v3/challenges/\w{16}$'
-
-    is_unranked = re.match(unranked_url, flow.request.url) is not None
-    is_ranked = re.match(ranked_url_pattern, flow.request.url) is not None
-    is_challenge = re.match(challenges_url_pattern, flow.request.url) is not None
-    if (
-        flow.request.method != 'GET'
-        or not is_ranked
-        and not is_unranked
-        and not is_challenge
-    ):
-        return
-
-    # We retrieve the response content in a dict
-    response_content = json.loads(flow.response.content.decode())
     geolocator = Nominatim(user_agent='Geoguessr')
-    if response_content:
-        if is_unranked or is_challenge:
-            game_mode = response_content['mode']
-            streak_type = response_content['streakType']
-            round_index = response_content['round'] - 1
-            current_round = response_content['rounds'][round_index]
-            latitude = current_round['lat']
-            longitude = current_round['lng']
-            coordinates = (latitude, longitude)
-            webbrowser.get(using='google-chrome').open_new_tab(
-                f'https://www.google.com/maps/place/{latitude},{longitude}',
-            )
-            if game_mode == 'streak':
-                if streak_type == 'usstatestreak':
-                    state = us.states.lookup(current_round['streakLocationCode'])
-                    print(f'{state.name}, {state.abbr}')
-                elif streak_type == 'countrystreak':
-                    country_code = current_round['streakLocationCode']
-                    country = pycountry.countries.get(alpha_2=country_code)
-                    print(country.name)
-            elif game_mode == 'standard':
-                location = geolocator.reverse(coordinates, language=LANGUAGE)
-                print(location.address)
-        elif is_ranked:
-            current_round = response_content['rounds'][-1]
-            latitude = current_round['lat']
-            longitude = current_round['lng']
-            coordinates = (latitude, longitude)
-            if response_content['isDistanceGame']:
-                webbrowser.get(using='google-chrome').open_new_tab(
-                    f'https://www.google.com/maps/place/{latitude},{longitude}',
-                )
-                location = geolocator.reverse(coordinates, language=LANGUAGE)
-                print(location.address)
-            else:
-                location = geolocator.reverse(coordinates, language=LANGUAGE)
-                print(location.address)
+    if re.match(std_pattern, url) and flow.request.method == 'GET':
+        response_content = json.loads(flow.response.content.decode())
+        lat = response_content['rounds'][-1]['lat']
+        lng = response_content['rounds'][-1]['lng']
+        coordinates = (lat, lng)
+        location = geolocator.reverse(coordinates, language='en', addressdetails=True)
+        display_location(location)
+
+    if re.match(adventure_pattern, url) and flow.request.method == 'POST':
+        match = re.match(adventure_pattern, url)
+        node_id = int(match.group(1))
+        response_content = json.loads(flow.response.content.decode())
+        for level in response_content.get('levels', []):
+            for node in level.get('nodes', []):
+                if node['id'] == node_id and 'game' in node:
+                    lat = node['game']['location']['lat']
+                    lng = node['game']['location']['lng']
+                    coordinates = (lat, lng)
+                    location = geolocator.reverse(coordinates, language='en', addressdetails=True)
+                    display_location(location)
 
 
 def websocket_message(flow):
-    message = flow.messages[-1]
-    if 'StreakNewRound' in message.content:
-        response = json.loads(message.content)
-        geolocator = Nominatim(user_agent='Geoguessr')
-        latitude = response['payload']['round']['lat']
-        longitude = response['payload']['round']['lng']
-        coordinates = (latitude, longitude)
-        location = geolocator.reverse(coordinates, language=LANGUAGE)
-        print(location.address)
+    """When playing in multiplayer, communication with the server is done via WebSockets"""
+    message = flow.websocket.messages[-1]
+    message_content = json.loads(message.content.decode())
+    geolocator = Nominatim(user_agent='Geoguessr')
+    if 'battleRoyaleGameState' in message_content and message_content['battleRoyaleGameState']:
+        round = message_content['battleRoyaleGameState']['rounds'][-1]
+        lat = round['lat']
+        lng = round['lng']
+        coordinates = (lat, lng)
+        location = geolocator.reverse(coordinates, language='en', addressdetails=True)
+        display_location(location)
